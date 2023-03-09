08da7c3c3ec15abc7d87f1cac4be4b55e9caf9ff
blockspector <94325444+blockspector@users.noreply.github.com>
2023-03-09 23:16:30 +0000

    Comit From Vs Code

---
+++ indexcapture.js
@@ -0,0 +1,7 @@
+// script for caputuring the about page email
+
+document.getElementById("myButton").onclick = function() {
+    var myEmail = document.getElementById("textInput").value
+    console.log(document.getElementById('textInput').value);
+    console.log(Hey);
+};  
